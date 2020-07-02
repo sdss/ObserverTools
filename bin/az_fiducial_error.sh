58178d4c982020cdcd84f221cfcb5162dd22fc14
Dylan Gatlin <dgatlin5@gmail.com>
2020-07-02 07:31:18 +0000

    Finalized a tested az_fiducial_error.sh

--- bin/az_fiducial_error.sh
+++ bin/az_fiducial_error.sh
@@ -8,10 +8,12 @@
 echo -n 'Enter mjd:'
 read mjd
 zippedFilename="/data/logs/mcp/mcpFiducials-"$mjd".dat.gz"
-if [[ -f zippedFilename ]]; then
-    filename=zippedFilename;
-    gzip -d --stdout ${filename} > temp.txt
-    tail -500  temp.txt | grep AZ_FIDUCIAL > temp.txt
+if [ -f $zippedFilename ]; then
+    echo "Reading a zipped file"
+    filename=$zippedFilename
+    gzip -d --stdout $filename > temp1.txt
+    tail -500  temp1.txt | grep AZ_FIDUCIAL > temp.txt
+    rm temp1.txt
 else
     filename="/data/logs/mcp/mcpFiducials-"$mjd".dat"
     tail -500  ${filename} | grep AZ_FIDUCIAL > temp.txt
@@ -22,3 +24,4 @@ let error1=$pos1-$exp1
 let error2=$pos2-$exp2
 echo "ID="$ID "two encoder errors are  " $error1 $error2
 done < temp.txt
+rm temp.txt
