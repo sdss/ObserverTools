9cf54c9ef292584f65119c82d3d004ac53a6fb72
Dylan Gatlin <dgatlin5@gmail.com>
2020-07-02 01:15:33 -0600

    Fized the az_*_error.sh scripts

--- bin/az_fiducial_error.sh
+++ bin/az_fiducial_error.sh
@@ -5,6 +5,17 @@
 #  Kaike Pan, APO.
 #------------------------------------------------------------------------------
 
+echo -n 'Enter mjd:'
+read mjd
+zippedFilename="/data/logs/mcp/mcpFiducials-"$mjd".dat.gz"
+if [[ -f zippedFilename ]]; then
+    filename=zippedFilename;
+    gzip -d --stdout ${filename} > temp.txt
+    tail -500  temp.txt | grep AZ_FIDUCIAL > temp.txt
+else
+    filename="/data/logs/mcp/mcpFiducials-"$mjd".dat"
+    tail -500  ${filename} | grep AZ_FIDUCIAL > temp.txt
+fi
 while read -r dump dump ID exp1 exp2 pos1 pos2 dump; do
 #echo $ID $exp1 $exp2
 let error1=$pos1-$exp1
