28946c636df6ff637b0bf7f0027e7e4b6ef11d0d
Dylan Gatlin <dgatlin@apo.nmsu.edu>
2021-11-23 00:58:29 +0000

    Versions handles idlspec2d

--- bin/versions.py
+++ bin/versions.py
@@ -17,41 +17,10 @@ __version__ = '3.1.0'
 
 def main():
     softwares, versions = [], []
-    # idlspec2d = sub.Popen('setup idlspec2d; idlspec2d_version', shell=True,
-                        #   stdout=sub.PIPE)
-    # softwares.append('idlspec2d')
-    # versions.append(idlspec2d.stdout.read().decode('utf-8').strip('\n'))
-
-    # plate_mapper = sub.Popen('setup idlmapper; echo "print, idlmapper_version()'
-                            #  '" | idl 2> /dev/null &', shell=True,
-                            #  stdout=sub.PIPE)
-    # softwares.append('plate-mapper3')
-    # versions.append(plate_mapper.stdout.read().decode('utf-8').strip('\n'))
-
-    # petunia = sub.Popen('readlink /home/sdss4/products/Linux64/petunia/current',
-                        # shell=True, stdout=sub.PIPE)
-    # softwares.append('Petunia')
-    # versions.append(petunia.stdout.read().decode('utf-8').strip('\n'))
-
-    # autoscheduler = sub.Popen('readlink /home/sdss4/products/Linux64/'
-                            #   'autoscheduler/current', shell=True,
-                            #   stdout=sub.PIPE)
-    # softwares.append('Autoscheduler')
-    # versions.append(autoscheduler.stdout.read().decode('utf-8').strip('\n'))
-
-    # sdss_module = sub.Popen("readlink /home/sdss4/products/Linux64/"
-                            # "sdss_python_module/current", shell=True,
-                            # stdout=sub.PIPE)
-    # softwares.append('SDSS Python Module')
-    # versions.append(sdss_module.stdout.read().decode('utf-8').strip('\n'))
-
-    # mangadrp = sub.Popen("/home/manga/products/Linux64/mangadrp/trunk/bin/"
-                        #  "mangadrp_version", shell=True, stdout=sub.PIPE)
-    # softwares.append('mangadrp')
-    # versions.append(mangadrp.stdout.read().decode('utf-8').strip('\n'))
 
     sdss_obstools = sub.run('pip list | grep sdss-obstools', shell=True,
-                              stdout=sub.PIPE).stdout.decode('utf-8').strip('\n')
+                              stdout=sub.PIPE
+                           ).stdout.decode('utf-8').strip('\n')
     if len(sdss_obstools) != 0:
         softwares.append(sdss_obstools.split()[0])
         versions.append(sdss_obstools.split()[-1])
@@ -73,11 +42,14 @@ def main():
     except:
         versions.append("FAILED")
     
-    has_module = bool(sub.run("module --help", shell=True, stdout=sub.PIPE,
+    has_module = sub.run("module --help", shell=True, stdout=sub.PIPE,
                               stderr=sub.PIPE
-                              ).stdout)
+                              ).stderr.decode("utf-8")
+    has_module = "command not found" not in has_module
+
     if has_module:
-        idlspec = sub.run("module load idlspec2d; idlspec2d_version", shell=True,
+        idlspec = sub.run("module load idlspec2d; idlspec2d_version",
+                          shell=True,
                           stdout=sub.PIPE).stdout.decode("utf-8").strip('\n')
         softwares.append("idlspec2d")
         versions.append(idlspec.split()[-1])
@@ -90,3 +62,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
