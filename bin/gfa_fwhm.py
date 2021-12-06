ecf38500008c9d6b5046157c1318512d1a1c74b6
Dylan Gatlin <dgatlin5@gmail.com>
2021-12-05 21:03:59 -0700

    Improved selection criteria in gfa_fwhm.py

--- bin/gfa_fwhm.py
+++ bin/gfa_fwhm.py
@@ -12,12 +12,45 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from pathlib import Path
+from matplotlib.patches import Ellipse
 
 from bin import sjd
 from sdssobstools import sdss_paths
 
 
 sns.set(style="darkgrid")
+
+
+def build_filt(obj_arr: np.ndarray):
+    ecc = np.sqrt(obj_arr['a']**2 - obj_arr['b']**2) / obj_arr['a']
+    # print(ecc, np.mean(ecc), np.percentile(ecc, 95))
+    filt = ((obj_arr["flux"] > np.percentile(obj_arr["flux"], 80))
+        & ((obj_arr["a"] * 0.216) > 0.6)
+        & (ecc < np.percentile(ecc, 75))
+        )
+    # print(obj_arr["a"] * 0.216, obj_arr["b"] * 0.216, np.mean([obj_arr["a"], obj_arr["b"]]))
+    return filt
+
+def show_img(obj_arr: np.ndarray, obj_filt, data_sub: np.ndarray, plot_file=""):
+    fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+    m, s = np.mean(data_sub), np.std(data_sub)
+    im = ax.imshow(data_sub, interpolation='nearest', cmap='gray',
+                   vmin=m-s, vmax=m+s, origin='lower')
+
+    # plot an ellipse for each object
+    for i, obj in enumerate(obj_arr[obj_filt]):
+        e = Ellipse(xy=(obj['x'], obj['y']),
+                    width=6*obj['a'],
+                    height=6*obj['b'],
+                    angle=obj['theta'] * 180. / np.pi)
+        e.set_facecolor('none')
+        e.set_edgecolor('red')
+        ax.add_artist(e)
+    plt.show()
+    if plot_file:
+        fig.savefig(plot_file)
+
+
 
 class GFASet:
     def __init__(self, verbose=False):
@@ -39,7 +72,7 @@ class GFASet:
             paths.append(p)
             if p.exists():
                 filter.append(True)
-                fwhm, n = self.get_fwhm(p, verbose=self.verbose)
+                fwhm, n, objs = self.get_fwhm(p, verbose=self.verbose)
                 fwhms.append(fwhm)
                 n_objs.append(n)
             else:
@@ -56,17 +89,15 @@ class GFASet:
     def get_fwhm(path: Path, verbose: bool=False):
         data = fitsio.read(path, 1)
         bkg = sep.Background(data.astype(float))
-        objects = sep.extract(data - bkg, 1.5, bkg.globalrms)
-        filt = ((objects['a'] / objects['b'] < 2)
-            & (objects['b'] / objects['a'] < 2)
-            & (objects["flux"] > np.percentile(objects["flux"], 80))
-            )
+        objs = sep.extract(data - bkg, 1.5, bkg.globalrms)
+        filt = build_filt(objs)
         if verbose:
             print(f"Using {filt.sum()}/{filt.shape[0]} points")
         fwhm = 0.216 * np.mean(2.0*np.sqrt(
-            2.0*np.log(2)*(objects['a'][filt]**2 + objects['b'][filt]**2))
+            2.0*np.log(2)*(objs['a'][filt]**2 + objs['b'][filt]**2))
         )
-        return fwhm, filt.sum()
+        return fwhm, filt.sum(), objs
+
 
     def sort(self):
         self.im_nums = np.array(self.im_nums)
@@ -134,6 +165,9 @@ def parse_args():
                         " only works with --plot and --window together. If not"
                         " included, the plot will show up in a new window (or"
                         " maybe not depending on X11).")
+    parser.add_argument("-d", "--plot-image", action="store_true",
+        help="Plot an the sky image with ellipses traced, only works with -f"
+    )
     args = parser.parse_args()
 
     return args
@@ -146,15 +180,23 @@ def main(args=None):
     if args.bias:
         raise NotImplementedError("This script doesn't support bias reduction")
     if args.file:
-        print(f"{'File Name':<20} {'FWHM':<6} {'N Objects':<9}")
+        print(f"{'File Name':<20} {'FWHM':<6} {'N Objects':<11} {'Mean Ecc':<6}")
         print('=' * 80)
         for file in args.file:
             if '*' not in file:
                 file = Path(file)
                 if not file.exists():
                     raise FileNotFoundError(f"Couldn't find {file.absolute()}")
-                fwhm, n_objs = gfas.get_fwhm(file)
-                print(f"{file.name:<20} {fwhm:>6.2f} {n_objs:>9.0f}")
+                fwhm, n_objs, objs = gfas.get_fwhm(file)
+                filt = build_filt(objs)
+                ecc = (np.sqrt(objs['a'][filt]**2 - objs['b'][filt]**2)
+                    / objs['a'][filt])
+                print(f"{file.name:<20} {fwhm:>6.2f} {filt.sum():>5.0f}"
+                    f"/{len(objs):<5.0f}  {np.mean(ecc):>6.2f}")
+                if args.plot_image:
+                    data = fitsio.read(file, 1)
+                    bkg = sep.Background(data.astype(float))
+                    show_img(objs, filt, data - bkg)
             else:
                 file = Path(file)
                 for p in file.parent.glob(file.name):
