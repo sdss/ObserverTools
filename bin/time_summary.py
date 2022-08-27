b1181eb11c3f3d43fc0219a16989b60e2f35d035
Dylan Gatlin <dgatlin@apo.nmsu.edu>
2022-08-27 05:47:46 +0000

    Switched time_summary verbose to an int

--- bin/time_summary.py
+++ bin/time_summary.py
@@ -17,7 +17,7 @@ from sdssobstools import sdss_paths
 
 def get_from_influx(name: str, query_name: str, influx_times,
                     out_dict: dict = {},
-                    verbose=False):
+                    verbose=0):
     q_path = Path(sdss_paths.__file__).parent.parent / "flux" / query_name
     if not q_path.exists():
         raise FileNotFoundError(
@@ -26,22 +26,22 @@ def get_from_influx(name: str, query_name: str, influx_times,
         q_query = fil.read()
 
     results = influx_fetch.query(q_query, influx_times.min(),
-                                 influx_times.max(), verbose=verbose)
+                                 influx_times.max(), verbose=verbose - 1)
     influx_times = []
     values = []
     if len(results) != 0:
         for row in results[0].records:
             v = row.get_value()
-            if len(values) >= 1:
-                if v == values[-1]:
-                    continue
+            # if len(values) >= 1:
+                # if v == values[-1]:
+                    # continue
             values.append(row.get_value())
             influx_times.append(row.get_time())
         influx_times = Time(influx_times)
         values = np.array(values)
         sorter = influx_times.argsort()
         influx_times = influx_times[sorter]
-        values = influx_times[sorter]
+        values = values[sorter]
     else:
         influx_times = Time([], format="isot")
         values = np.array([])
@@ -76,10 +76,10 @@ def gen_summary(times, no_enclosure, verbose):
     tstart = Time.now()
     encl_p = mp.Process(target=get_from_influx,
                         args=("Enclosure", "enclosure.flux", k_times, influx_vals,
-                              verbose >= 2))
+                              verbose - 1))
     sci_p = mp.Process(target=get_from_influx,
                        args=("Science", "science_exposures.flux", k_times,
-                             influx_vals, verbose >= 2))
+                             influx_vals, verbose - 1))
     encl_p.start()
     sci_p.start()
     encl_p.join(5)
@@ -87,10 +87,17 @@ def gen_summary(times, no_enclosure, verbose):
     tend = Time.now()
     if verbose >= 1:
         print(f"Influx queries took {(tend - tstart).sec:.1f}s")
-    if verbose >= 2:
+    if verbose >= 3:
         print("Enclosure and Science times:")
         print(influx_vals["tEnclosure"])
         print(influx_vals["tScience"])
+    
+    if len(influx_vals["tEnclosure"]) > 0:
+        influx_vals["tEnclosure"] = influx_vals["tEnclosure"][:-1][
+            np.diff(influx_vals["Enclosure"]) != 0]
+        influx_vals["Enclosure"] = influx_vals["Enclosure"][:-1][
+            np.diff(influx_vals["Enclosure"]) != 0]
+
     time_tracking = {}
     time_tracking["BrightTotal"] = k_times[3] - k_times[2]
     time_tracking["DarkTotal"] = k_times[1] - k_times[0]
@@ -98,22 +105,24 @@ def gen_summary(times, no_enclosure, verbose):
     time_tracking["DarkLost"] = TimeDelta(0, format="sec")
     time_tracking["BrightObserved"] = TimeDelta(0, format="sec")
     time_tracking["DarkObserved"] = TimeDelta(0, format="sec")
+    time_tracking["DarkTech"] = TimeDelta(0, format="sec")
+    time_tracking["BrightTech"] = TimeDelta(0, format="sec")
 
     surveys = ("Dark", "Bright")
     for i in range(len(k_times)//2):
         total_time = TimeDelta(0, format="sec")
         low, high = k_times[i * 2: (i + 1) * 2]
-        for j, encl_change in enumerate(influx_vals["Enclosure"]):
-            if encl_change == -1:
+        for j, encl in enumerate(influx_vals["Enclosure"]):
+            if encl == -1:
                 continue
             if not no_enclosure:
                 start = influx_vals["tEnclosure"][j]
                 sci_window = influx_vals["tScience"] > influx_vals["tEnclosure"][j]
             else:
-                start = 0
-                sci_window = influx_vals["tScience"]
+                start = low
+                sci_window = influx_vals["tScience"] > low
             if len(influx_vals["tEnclosure"]) > j + 1 and not no_enclosure:
-                sci_window = sci_window & (sci_window
+                sci_window = sci_window & (influx_vals["tScience"]
                                            < influx_vals["tEnclosure"][j + 1])
             t_sci = influx_vals["tScience"][sci_window]
 
@@ -130,44 +139,61 @@ def gen_summary(times, no_enclosure, verbose):
                     continue  # No science was taken after opening/twilight
 
             if len(t_sci) != 0:
-                end = t_sci[-1] + 15 / 60 / 24  # Science ends 15 minutes
+                end = t_sci[-1] + 8 / 60 / 24  # Science ends 8 minutes
                 # after last exposure started
             if not no_enclosure:
                 if len(influx_vals["tEnclosure"]) > j + 1:
                     end = max(influx_vals["tEnclosure"][j + 1], end)
             if np.abs((high - end).sec / 60) > 15:
                 print(f"{surveys[i]} ended 15m away from end of window")
-            total_time += end - start
+                
+            in_window = ((influx_vals["tScience"] > start) &
+                         (influx_vals["tScience"] < end))
+            dts_mins = np.diff(influx_vals["tScience"][in_window].mjd) * 24 * 60
+            wasted_time = TimeDelta(np.sum((dts_mins[dts_mins > 23] - 16) * 60),
+                                    format="sec")
+            if verbose >= 2:
+                print(f"Survey {surveys[i]} adding from {start.iso} - {end.iso}"
+                      f" with {wasted_time.sec / 3600:.1f} wasted time"
+                      )
+            total_time += (end - start
+                           - wasted_time
+                           )
+            time_tracking[surveys[i] + "Tech"] += wasted_time
         time_tracking[
             surveys[i] + "Observed"] = (time_tracking[surveys[i] + "Observed"]
                                     + total_time)
         time_tracking[
             surveys[i] + "Observed"] = max(time_tracking[surveys[i] + "Observed"], 0)
     time_tracking["BrightLost"] = (time_tracking["BrightTotal"]
-                                   - time_tracking["BrightObserved"])
+                                   - time_tracking["BrightObserved"]
+                                   - time_tracking["BrightTech"])
     time_tracking["DarkLost"] = (time_tracking["DarkTotal"]
-                                   - time_tracking["DarkObserved"])
+                                   - time_tracking["DarkObserved"]
+                                   - time_tracking["DarkTech"])
 
     summary = "*=*=*= Observatory: APO\n"
     summary += f"*=*=*= MJD: {k_times.min().mjd:.0f}\n"
     summary += "       Fill night time in Hours below\n"
-    summary += f"*=*=*=  Total bright time                     :"
+    summary += f"*=*=*= Total bright time                     :"
     summary += f" {time_tracking['BrightTotal'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Bright time, queued observations      :"
+    summary += f"*=*=*= Bright time, queued observations      :"
     summary += f" {time_tracking['BrightObserved'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Bright time, commissioning/engineering: 0\n"
-    summary += f"*=*=*=  Bright time lost to weather           :"
+    summary += f"*=*=*= Bright time, commissioning/engineering: 0\n"
+    summary += f"*=*=*= Bright time lost to weather           :"
     summary += f" {time_tracking['BrightLost'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Bright time lost to technical issues  : 0\n"
+    summary += f"*=*=*= Bright time lost to technical issues  :"
+    summary += f" {time_tracking['BrightTech'].sec / 3600:.1f}\n"
     summary += f"\n"
-    summary += f"*=*=*=  Total dark time                       :"
+    summary += f"*=*=*= Total dark time                       :"
     summary += f" {time_tracking['DarkTotal'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Dark time, queued observations        :"
+    summary += f"*=*=*= Dark time, queued observations        :"
     summary += f" {time_tracking['DarkObserved'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Dark time, commissioning/engineering  : 0\n"
-    summary += f"*=*=*=  Dark time lost to weather             :"
+    summary += f"*=*=*= Dark time, commissioning/engineering  : 0\n"
+    summary += f"*=*=*= Dark time lost to weather             :"
     summary += f" {time_tracking['DarkLost'].sec / 3600:.1f}\n"
-    summary += f"*=*=*=  Dark time lost to technical issues    : 0\n"
+    summary += f"*=*=*= Dark time lost to technical issues    :"
+    summary += f" {time_tracking['DarkTech'].sec / 3600:.1f}\n"
 
     return summary
 
@@ -191,5 +217,5 @@ if __name__ == "__main__":
     # range, so using now seems to cover the bases.
     for i, arg in enumerate(sys.argv):
         if arg == "--":
-            sys.argv[i] = Time.now().iso
+            sys.argv[i] = (Time.now() + 0.2).iso
     print_summary()
--- flux/science_exposures.flux
+++ flux/science_exposures.flux
@@ -1,18 +1,10 @@
-from(bucket: "apo-medium-retention")
+from(bucket: "actors")
     |> range(start: v.timeRangeStart, stop: v.timeRangeStop)
     |> filter(fn: (r) => (
-            r.actor == "boss"
-            and r._measurement == "exposureState"
-            and r._field == "state"
-            and r._value == "INTEGRATING"
-        // These two queries seem to contradict each other, not sure why
-        // or (
-            // r.actor == "apogee"
-            // and r._measurement == "exposureState"
-            // and r._field == "expType"
-            // and r._value == "Object"
-            // )
+            r._measurement == "apogee"
+            and r._field == "exposureState_expType"
+            and r._value == "Object"
         )
     )
-    |> aggregateWindow(every:15m, fn:last, createEmpty: false)
-    |> yield()
+    // |> aggregateWindow(every:1m, fn:last, createEmpty: true)
+    // |> yield()
